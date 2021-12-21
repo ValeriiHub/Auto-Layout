603d38d6180005227b1bd2aa377ceea9cbdb52a1
Valerii <teslareiden@gmail.com>
2021-12-21 20:42:27 +0200

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Valerii D on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
