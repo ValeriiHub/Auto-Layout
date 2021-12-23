098f3f39c6961437192fbbeea2ce1319a047620b
Valerii <teslareiden@gmail.com>
2021-12-23 14:31:46 +0200

    Add stackView

--- Auto Layout/ViewController.swift
+++ Auto Layout/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Auto Layout
+//  AutoLayout
 //
-//  Created by Valerii D on 21.12.2021.
+//  Created by Valerii Dulia on 21.12.2021.
 //
 
 import UIKit
@@ -13,42 +13,110 @@ class ViewController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "bear_first"))
         //this enables autolayout for our imageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
+        
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!",
+                                                       attributes: [.font : UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready forloads and loads of fun?",
+                                                 attributes: [.font : UIFont.boldSystemFont(ofSize: 13),
+                                                              .foregroundColor : UIColor.gray]))
+        
+        textView.attributedText = attributedText
         textView.textAlignment = .center
-        textView.font = .boldSystemFont(ofSize: 18)
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isEditable = false
         textView.isScrollEnabled = true
         return textView
     }()
     
+    let topImageContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        //        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        button.setTitleColor(pinkColor, for: .normal)
+        //        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(bearImageView)
+        view.addSubview(topImageContainerView)
+        topImageContainerView.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
+        view.addSubview(previousButton)
         
         setupLoyout()
+        setupBottomControls()
+    }
+    
+    func setupBottomControls() {
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.distribution = .fillEqually
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     private func setupLoyout() {
         NSLayoutConstraint.activate([
-            bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100 ),
+            bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+            bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
             bearImageView.widthAnchor.constraint(equalToConstant: 200),
-            bearImageView.heightAnchor.constraint(equalToConstant: 200)
+            bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5)
         ])
         
         NSLayoutConstraint.activate([
-            descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 100 ),
-            descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor),
+            descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
+            descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
             descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        NSLayoutConstraint.activate([
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+        
+        
     }
 }
